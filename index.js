8d58a8befe798701459bba9c72d161af1eab6003
Charming Darling <h1hasinai_f06@yahoo.com>
2023-08-27 11:46:14 -0700

    Added generateSBG to required scripts. Changed intialization function to askUser();

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Scripts, requiring these scripts
-const generateSVG = require("");
+const generateSVG = require(".lib/generateShape");
 const questions = require("./lib/questions");
-const { Circle, Triangle, Square } = require("./lib/shapes.js");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 function init() {
-  // Promise STARTED, is executing work sometime, but the work is NOT completed
+  // Promise STARTED, is executing work sometime, but the work is NOT completed at this time
   inquirer.promptScriptQuestions(questions);
-  console.log("Starting to ask questions"); //
+  console.log("Starting to ask questions");
 }
 // Async prepares node to tell them that this next function has an "await" and it needs to wait. This promise may get rejected or resolved.
 // (answers) is the parameter pointing to the function
@@ -18,18 +18,16 @@ const askUser = async (answers) => {
   // Promise is COMPLETED here
   const answers = await inquirer.promptScriptQuestions(questions);
   console.log(answer);
+  const createShape = "Creating Shape...";
+  fs.writeFile("/assets/svglogo.png", createShape, (err) =>
+    err
+      ? console.error("Error. SVG Logo not created.", err)
+      : console.log("You created an SVG logo.")
+  );
 };
-
-const createShape = "Creating Shape";
-fs.writeFile("/assets/svglogo.png", createShape, (err) =>
-  err
-    ? console.error("Error. SVG Logo not created.", err)
-    : console.log("You created an SVG logo.")
-);
-
-init();
+// Function to initialize
+askUser();
 
 // Promises (rejected, resolved) : When something gives you back a promise and if it is run it will PASS OR FAIL.
-
 // PASS = Resolved
 // FAIL = Rejected
