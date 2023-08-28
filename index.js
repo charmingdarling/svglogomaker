5711366193de7e60ffc10b3d103c82005f2e9b75
Charming Darling <h1hasinai_f06@yahoo.com>
2023-08-28 16:51:27 -0700

    Edited writemefile to work with no strings around svgContent.

--- index.js
+++ index.js
@@ -14,24 +14,38 @@ async function init() {
     case "circle":
       console.log("User has chosen the circle shape.");
       // Creating a new circle instance
-      createShapeObj = new Circle();
+      createShapeObj = new Circle(
+        answers.text,
+        answers.textColor,
+        answers.shapeColor
+      );
       break;
     case "triangle":
       console.log("User has chosen the triangle shape.");
-      createShapeObj = new Triangle();
+      createShapeObj = new Triangle(
+        answers.text,
+        answers.textColor,
+        answers.shapeColor
+      );
       break;
     case "square":
       console.log("User has chosen the square shape.");
-      createShapeObj = new Square();
+      createShapeObj = new Square(
+        answers.text,
+        answers.textColor,
+        answers.shapeColor
+      );
       break;
     default:
       console.log("Error. Unsupported shape.");
       return;
   }
 
+  //! the svgContent is not generating it correctly
   const svgContent = createShapeObj.render();
+  console.log(svgContent);
 
-  fs.writeFile("./assets/logo.svg", "svgContent", (err) =>
+  fs.writeFile("./assets/logo.svg", svgContent, (err) =>
     err
       ? console.error("Error. SVG Logo not created.", err)
       : console.log("You created an SVG logo.")
